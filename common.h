ed0c6dea01bb6aee481c488fbf889607f8e7322f
fdd_linux <672575302@qq.com>
2017-06-23 01:12:44 -0700

    fitst

---
+++ common.h
@@ -0,0 +1,7 @@
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+int add(int a, int b);
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "common.h"
+
+int main(void)
+{
+	printf("%d\n", add(1, 2));
+}
